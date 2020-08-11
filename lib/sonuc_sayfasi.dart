0dd5352356f3907a9a1a67afa60add32cb71e8c3
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-11 12:05:54 +0300

    sonuc_sayfasinin_tasarimi

--- lib/sonuc_sayfasi.dart
+++ lib/sonuc_sayfasi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'hesap_card.dart';
 
 class SonucSayfasi extends StatelessWidget {
   @override
@@ -7,7 +8,50 @@ class SonucSayfasi extends StatelessWidget {
       appBar: AppBar(
         title: Text("VKI Hesaplama Sonuçları"),
       ),
-      body: Text("Sonuç"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Center(
+            child: Expanded(
+                child: Container(
+              child: Text(
+                "Sonucunuz:",
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              ),
+            )),
+          ),
+          Expanded(
+              flex: 5,
+              child: HesapCard(
+                renk: Color.fromRGBO(255, 184, 140, 1),
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Normal",
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          color: Color.fromRGBO(77, 59, 59, 1),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "15",
+                      style: TextStyle(
+                          fontSize: 100.0,
+                          color: Color.fromRGBO(77, 59, 59, 1)),
+                    ),
+                    Text(
+                      "Yorum",
+                      style: TextStyle(
+                          fontSize: 20.0, color: Color.fromRGBO(77, 59, 59, 1)),
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
